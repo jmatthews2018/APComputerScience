0b9140c2485b5cb3ae86be9ebcc1be3da3ecd6e0
jmatthews2018 <jmatthews2018@cchsraider.net>
2017-09-21 13:55:05 -0400

    first commit

---
+++ Problem2/src/Problem2.java
@@ -0,0 +1,20 @@
+/*
+ * John Matthews
+ * Problem 2
+ * Write a program to output a text 
+ */
+public class Problem2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//This is the program to output the text
+		System.out.println("A well-formed Java program has");
+		System.out.println("a main method with { and }");
+		System.out.println("braces.\n"); //We need a space between this and the next paragraph so \n will create a new line for us
+		System.out.println("A System.out.println statement");
+		System.out.println("has ( and ) and usually a");
+		System.out.println("String enclosed between \" and \""); // \" needed to display "
+		
+	}
+
+}
