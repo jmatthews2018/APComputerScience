8d7424bcd98c3701cc619e34b8e5f258f4fc3331
jmatthews2018 <jmatthews2018@cchsraider.net>
2017-09-21 13:41:09 -0400

    initial commit

---
+++ Problem1/src/Problem1.java
@@ -0,0 +1,19 @@
+/*
+ * John Matthews
+ * Problem 1
+ * Display a message that says victory is mine
+ */
+public class Problem1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//displays victory is mine with a box around it
+		topBottom();
+		System.out.println("|| Victory is mine! ||");
+		topBottom();
+	}
+public static void topBottom()
+{ //displays the top a bottom lines of the box
+	System.out.println("//////////////////////");
+}
+}
